7841cfdb2ae534a3f802e87ddfef5313101a73b6
bacowan <bacowan@ualberta.ca>
2015-03-09 21:08:09 -0600

    some gem creating things

---
+++ ParallelMergeSort.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'ParallelMergeSort'
+  s.version = '0.0.0'
+  s.date = '2015-03-09'
+  s.summary = "Parallel merge sort"
+  s.description = "parallel merge sort"
+  s.authors = ["Brendan Cowan","Ryan Kusters"]
+  s.email = 'bacowan@ualberta.ca'
+  s.files = ["lib/asgn3.rb","lib/ParallelMergeSort.rb","lib/testRunner.rb"]
+  s.homepage =
+      'https://github.com/bacowan/ECE421_assignment3'
+  s.license = 'N/A'
+end
---
+++ lib/asgn3.rb
@@ -0,0 +1,59 @@
+
+require 'timeout'
+require_relative './ParallelMergeSort.rb'
+
+class MyTimeOutException < Exception
+end
+
+class MyDecorator
+  include Comparable
+  @myObject
+  @block
+  def initialize(object,block)
+    @myObject = object
+    @block = block
+  end
+  def <=>(other)
+    @block.call(@myObject,other.getObject)
+  end
+  def getObject
+    return @myObject
+  end
+end
+
+module Sorter
+  def unlimited_Sort(my_list,duration,&block)
+
+    #preconditions
+    raise 'Items to sort must implement .each' unless my_list.respond_to?(:each)
+
+    if(not block.nil?)
+      my_list2 = Array.new
+      my_list.each{|element| my_list2.push(MyDecorator.new(element,block)) }
+    else
+      my_list2 = my_list
+    end
+
+    result = my_list
+    begin
+      sorter = ParaMergeSort.new(my_list2)
+      status = Timeout::timeout(duration,MyTimeOutException){
+        sorter.pMergeSort(0,my_list.size-1)
+      }
+    rescue MyTimeOutException
+      puts 'time ran out'
+    end
+    if sorter.getArray[0].is_a?(MyDecorator)
+      finalList = Array.new
+      sorter.getArray.each{|element| finalList.push(element.getObject) }
+    else
+      finalList = sorter.getArray
+    end
+    return finalList
+  end
+end
+
+
+things = [1,2,3,7,5]
+include Sorter
+puts unlimited_Sort(things,1000)#{|a,b| b <=> a}
